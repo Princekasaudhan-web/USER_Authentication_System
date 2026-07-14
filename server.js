0de91f2f7d1b205e509fa779529b75c87891c04b
Prince <princekasaudhan11indian@gmail.com>
2026-07-14 15:25:56 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
+
+dotenv.config();
+
+connectDB();
+
+
+const app = express();
+
+
+app.use(express.json());
+app.use(cors());
+
+app.post("/test", (req, res) => {
+  console.log("Body:", req.body);
+  res.json(req.body);
+});
+
+
+app.use("/api/auth", authRoutes);
+
+
+app.get("/", (req, res) => {
+  res.send("User Authentication API Running...");
+});
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(` Server running on port ${PORT}`);
+});
